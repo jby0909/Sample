1ab48e79b1b20d9d0258a6d751c9f90313920513
jby0909 <56112547+jby0909@users.noreply.github.com>
2025-03-18 09:39:08 +0900

    message 보내기

---
+++ L20250318/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = new JObject();
+            json.Add("message" , "안녕하세요");
+
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.26"), 4000);
+
+            serverSocket.Connect(serverEndPoint);
+
+            byte[] buffer;
+
+            
+            buffer = Encoding.UTF8.GetBytes(json.ToString());
+
+            
+            int SendLength = serverSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+            
+            byte[] buffer2 = new byte[1024];
+            
+            int recvLength = serverSocket.Receive(buffer2);
+
+            Console.WriteLine(Encoding.UTF8.GetString(buffer2));
+            Console.ReadKey();
+
+            serverSocket.Close();
+
+        }
+    }
+}
---
+++ L20250318/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.26"), 4000);
+            listenSocket.Bind(listenEndPoint);
+
+            listenSocket.Listen(10);
+
+            while(true)
+            {
+                Socket clientSocket = listenSocket.Accept();
+
+                byte[] buffer = new byte[1024];
+                int recvLength = clientSocket.Receive(buffer);
+
+                Console.WriteLine(Encoding.UTF8.GetString(buffer));
+
+                if(recvLength <= 0)
+                {
+                    break;
+                }
+
+                var recvJson = new JObject();
+                recvJson.Add("message", "안녕하세요");
+                if (Encoding.UTF8.GetString(buffer).CompareTo(recvJson.ToString()) == 0)
+                {
+                    var json = new JObject();
+                    json.Add("message", "반가워요");
+                    buffer = Encoding.UTF8.GetBytes(json.ToString());
+
+                    int sendLength = clientSocket.Send(buffer);
+                    if (sendLength <= 0)
+                    {
+                        break;
+                    }
+                }
+                
+
+
+                clientSocket.Close();
+            }
+            listenSocket.Close();
+        }
+    }
+}
